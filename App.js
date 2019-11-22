4022d95dcfe2801dedac73a2771b5602f6aa8f75
Alpha Oumar Barry <alpha-oumar.barry@epitech.eu>
2019-11-22 15:28:03 +0100

    ajout fonction de log et de BDD

--- App.js
+++ App.js
@@ -1,7 +1,107 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase, { firestore } from 'firebase';
+import config from './config'
+
+require("firebase/firestore");
+
+// initialise firebase -------------------
+
+ firebase.initializeApp(config);
+
+// Autentification ---------------------------------------------------------------------------------------------------------------------------------------
+
+// créer un compte ---------
+
+function createUser(username, password) {
+  const user = firebase.auth().createUserWithEmailAndPassword(username, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage);
+    // ...
+  }, {me});
+  return user.uid;
+}
+
+// se connecter -----------
+
+async function signIn(username, password) {
+  const user = await firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("error");
+    // ...
+  });
+  return user.uid;
+}
+
+// se deconnecter ----------
+
+function signOut() {
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });
+}
+
+// BDD ---------------------------------------------------------------------------------------------------------------------------------------
+
+// ajouter une tache
+
+function setTodo(text) {
+  let doc = firebase.firestore().collection("todolist").doc();
+  doc.set({
+    docId: doc.id,
+    uid: firebase.auth().currentUser.uid,
+    todo: text,
+    validate: false
+  }, { merge: true });
+}
+
+// modifier une tache
+
+function updateTodo(docId, text) {
+  firebase.firestore().collection("todolist").doc(docId).update({
+    todo: text,
+  });
+}
+
+// supprimer une tache
+
+function deleteTodo(docId){
+  firebase.firestore().collection("todolist").doc(docId).delete();
+}
+
+// ecoute quand un evenement qui se produit dans la BDD
+
+function listenEventToDo() {
+  firebase.firestore().collection("todolist").where("uid", "==", firebase.auth().currentUser.uid).onSnapshot(function(docs) {
+    docs.docChanges().forEach(function (change) {
+      if (change.type === "added") {
+        console.log("New city: ", change.doc.data());
+      }
+      if (change.type === "modified") {
+        console.log("Modified city: ", change.doc.data());
+      }
+      if (change.type === "removed") {
+        console.log("Removed city: ", change.doc.data());
+      }
+    });
+   });
+}
+
 
 export default function App() {
+
+ signIn("alpha-oumar@hotmail.fr", "123456").then(function(uid){
+  //setTodo("hello world");
+  //updateTodo("iKy8ymiEjEwEpm6KN5Y6", "hehehehehe");
+  listenEventToDo();
+ });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyAfIa2XQUFoPV-SvXqpWH6gp0fCOtuaOZE",
+  authDomain: "todolist-6f6b4.firebaseapp.com",
+  databaseURL: "https://todolist-6f6b4.firebaseio.com",
+  projectId: "todolist-6f6b4",
+  storageBucket: "todolist-6f6b4.appspot.com",
+  messagingSenderId: "917739080909",
+  appId: "1:917739080909:web:99402f320d3ea776fb6d84",
+  measurementId: "G-1Q8F71GMQT"
+  };
